a6bdfcec55304d9aa96f6deab9bff8d7a0fa8499
Inna Antypova <nightallatiel@gmail.com>
2023-10-24 09:46:26 +0300

    add vacancies.py

---
+++ src/vacancies.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+class Vacancies(ABC):
+    """ Абстрактный класс для работы с API сайтов с вакансиями."""
+
+    @abstractmethod
+    def get_vacancies(self):
+        """
+        Метод для получения вакансий
+        """
+        pass
+

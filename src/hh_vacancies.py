7aeb0248d6eaaebc5383d4cfb92034bdaa000832
Inna Antypova <nightallatiel@gmail.com>
2023-10-26 15:22:12 +0300

    add methods

--- src/hh_vacancies.py
+++ src/hh_vacancies.py
@@ -1,9 +1,10 @@
-from src.vacancies import Vacancies
+from src.vacancies_abc import ABCVacancies
 import requests
-import json
+from datetime import datetime
+from src.vacancies import Vacancy
 
 
-class HHVacancies(Vacancies):
+class HeadHunterAPI(ABCVacancies):
     """ Класс для работы с API сайта hh.ru """
 
     HH_url = "https://api.hh.ru/vacancies"
@@ -19,12 +20,43 @@ class HHVacancies(Vacancies):
                  'page': 0,
                  'per_page': 100}  # кол-во вакансий
 
-        response = requests.get(url=HHVacancies.HH_url, headers={"User-Agent": "HH-User-Agent"}, params=param).json()
-        return response
+        response = requests.get(url=HeadHunterAPI.HH_url, headers={"User-Agent": "HH-User-Agent"}, params=param).json()
+
+        vacancies_list = []
+        for item in response['items']:
+            vacancies_list.append(item)
+
+        return vacancies_list
 
     def __repr__(self):
         return self.job_title
 
+    @staticmethod
+    def get_vacancy(vacancies_list):
+        for vacancy in vacancies_list:
+            if vacancy['address']:
+                address = vacancy['address']['raw']
+            else:
+                address = vacancy['address']
 
-hh = HHVacancies('Python')
-print(hh.get_vacancies())
+            vacancy_name = vacancy['name']
+            area = vacancy['area']['name']
+            employment = vacancy['employment']['name']
+            id_v = vacancy['id']
+            published = vacancy['published_at']
+            requirement = vacancy['snippet']['requirement']
+            responsibility = vacancy['snippet']['responsibility']
+
+            if vacancy['salary']:
+                salary = f"{vacancy['salary']['from']} - {vacancy['salary']['to']} {vacancy['salary']['currency']}"
+            else:
+                salary = vacancy['salary']
+
+            vacancy = Vacancy(vacancy_name, area, salary, address, published, requirement, responsibility,
+                              employment, id_v)
+            Vacancy.vacancy_obj_list.append(vacancy)
+
+
+# hh = HeadHunterAPI('Python')
+# print(hh.get_vacancies())
+# print(len(hh.get_vacancies()))

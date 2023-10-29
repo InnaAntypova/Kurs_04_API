d847630a78bd4a96756166e1a0e158cf80c090cc
Inna Antypova <nightallatiel@gmail.com>
2023-10-29 21:59:08 +0300

    add hh_vacancies.py

--- src/hh_vacancies.py
+++ src/hh_vacancies.py
@@ -1,7 +1,6 @@
 from src.vacancies_abc import ABCVacancies
 import requests
 from datetime import datetime
-from src.vacancies import Vacancy
 
 
 class HeadHunterAPI(ABCVacancies):
@@ -12,51 +11,49 @@ class HeadHunterAPI(ABCVacancies):
     def __init__(self, job_title: str):
         self.job_title = job_title  # Название вакансии
 
-    def get_vacancies(self):
-        """ Метод для получения вакансий """
-
+    def get_from_api(self):
+        """ Метод для получения ответа от API """
         param = {'text': self.job_title,
                  'area': 76,  # ищем в Ростове-на-Дону
                  'page': 0,
                  'per_page': 100}  # кол-во вакансий
 
-        response = requests.get(url=HeadHunterAPI.HH_url, headers={"User-Agent": "HH-User-Agent"}, params=param).json()
+        headers = {"User-Agent": "HH-User-Agent"}
 
-        vacancies_list = []
-        for item in response['items']:
-            vacancies_list.append(item)
+        response = requests.get(url=HeadHunterAPI.HH_url, headers=headers, params=param).json()['items']
+        return response
 
-        return vacancies_list
-
-    def __repr__(self):
-        return self.job_title
-
-    @staticmethod
-    def get_vacancy(vacancies_list):
-        for vacancy in vacancies_list:
-            if vacancy['address']:
-                address = vacancy['address']['raw']
-            else:
-                address = vacancy['address']
-
-            vacancy_name = vacancy['name']
-            area = vacancy['area']['name']
-            employment = vacancy['employment']['name']
-            id_v = vacancy['id']
-            published = vacancy['published_at']
-            requirement = vacancy['snippet']['requirement']
-            responsibility = vacancy['snippet']['responsibility']
-
+    def get_vacancy(self) -> list:
+        """ Метод собирает из необходимых данных лист с вакансиями"""
+        all_hh_vacancies = []
+        data = self.get_from_api()
+        for vacancy in data:
             if vacancy['salary']:
-                salary = f"{vacancy['salary']['from']} - {vacancy['salary']['to']} {vacancy['salary']['currency']}"
+                salary_from = vacancy['salary']['from'] if vacancy['salary']['from'] else 0
+                salary_to = vacancy['salary']['to'] if vacancy['salary']['to'] else 0
             else:
-                salary = vacancy['salary']
+                salary_from = 0
+                salary_to = 0
 
-            vacancy = Vacancy(vacancy_name, area, salary, address, published, requirement, responsibility,
-                              employment, id_v)
-            Vacancy.vacancy_obj_list.append(vacancy)
+            if vacancy['address']:
+                address = vacancy['address']['raw'] if vacancy['address']['raw'] else ''
+            else:
+                address = ''
 
-
-# hh = HeadHunterAPI('Python')
-# print(hh.get_vacancies())
-# print(len(hh.get_vacancies()))
+            published = datetime.fromisoformat(vacancy['published_at']).strftime('%d-%m-%Y')
+            responsibility = vacancy['snippet']['responsibility'] if vacancy['snippet']['responsibility'] else ''
+            requirement = vacancy['snippet']['requirement'] if vacancy['snippet']['requirement'] else ''
+            all_hh_vacancies.append({
+                'address': address,
+                'salary_from': salary_from,
+                'salary_to': salary_to,
+                'published': published,
+                'vacancy_url': vacancy['alternate_url'],
+                'vacancy_name': vacancy['name'],
+                'area': vacancy['area']['name'],
+                'employment': vacancy['employment']['name'],
+                'requirement': requirement,
+                'responsibility': responsibility,
+                'platform': 'HeadHunter'
+            })
+        return all_hh_vacancies
